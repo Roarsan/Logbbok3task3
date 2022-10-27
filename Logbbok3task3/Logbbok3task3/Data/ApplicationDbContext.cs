e16495849f109d4410eba138c29d7e13bb6b9258
Roshan Gauchan <rgauchan3@gmail.com>
2022-10-27 10:52:09 +0100

    Add project files.

---
+++ Logbbok3task3/Logbbok3task3/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logbbok3task3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
